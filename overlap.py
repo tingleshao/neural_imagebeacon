d29ff3fe1a4a769d10da0b04cde33bee35e5e1fd
marbleimplementer <marbleimplementer@gmail.com>
2017-05-03 14:09:59 -0400

    update the python script, but it still buggy

--- overlap.py
+++ overlap.py
@@ -3,4 +3,20 @@ import cv2
 import numpy as np
 
 
-def overlap(bin_img, backgrund_img, foreground_img):
+def overlap(bin_img, background_img, foreground_img):
+    background_img = cv2.multiply(background_img, bin_img)
+    foreground_img = cv2.multiply(cv2.subtract(np.ones(64,64), bin_img), foreground_img)
+    result_img = cv2.add(background_img, foreground_img)
+    return result_img
+
+
+def main():
+    foreground_img = cv2.imread(sys.argv[1])
+    backgrund_img = cv2.imread(sys.argv[2])
+    bin_img = cv2.imread(sys.argv[3])
+    result_img = overlap(bin_img, backgrund_img, foreground_img)
+    cv2.imwrite("result.jpg", result_img)
+
+
+if __name__ == "__main__":
+    main()
