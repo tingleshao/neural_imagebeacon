593decde921e01138c3dde913c1a375c7ee12001
tingleshao <cshao@cs.unc.edu>
2017-05-25 00:27:48 -0400

    add new file running exp

---
+++ run_exp.py
@@ -0,0 +1,10 @@
+# runs the experiment:
+# two stages:
+
+# style transfer
+
+# blending
+
+
+# future stages:
+# foreground/background detection -> upscale -> style transfer (color preserving) -> blending
