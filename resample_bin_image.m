a85b604f2d1eefcf496267a05bf7449b9304effe
tingleshao <cshao@cs.unc.edu>
2017-05-25 00:24:02 -0400

    add matlab program to resize image

--- resample_bin_image.m
+++ resample_bin_image.m
@@ -1,9 +1,13 @@
-function [img] = resample_bin_image(file_name, w, h)
+function [img_output] = resample_bin_image(file_name, w, h)
 
     img = imread(['neural_imagebeacon_data/', file_name, '.png']);
     img = rgb2gray(img);
     img = imresize(img, [w, h], 'nearest');
     img(img > 100) = 255;
     img(img <= 100) = 0;
-    imwrite(img, [file_name, num2str(w), 'x', num2str(h), '.png']);
+    
+    img_output(:,:,1) = img;
+    img_output(:,:,2) = img;
+    img_output(:,:,3) = img;
+    imwrite(img_output, [file_name, num2str(w), 'x', num2str(h), '.png']);
 end
