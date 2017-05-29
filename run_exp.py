4d62e720dd94fd042e5496da48e477fa6290578d
tingleshao <cshao@cs.unc.edu>
2017-05-28 22:57:39 -0400

    update script

--- run_exp.py
+++ run_exp.py
@@ -8,13 +8,17 @@ style_img_dir = ''
 output_name = ''
 # style transfer
 # change dir
+cwd = os.getcwd()
 os.chdir(neural_doodle_dir)
 os.system("python3 doodle.py --content {0} --style {1} --output {2} --phases=1 \
 --smoothness={3} --seed=\"content\" --iterations=500 --content-weight={} \
 --style-weight={} --semantic-weight={}".format(content_img_dir, style_img_dir, \
 output_name, smoothness, content_weight, style_weight, semantic_weight))
 
+os.chdir(cwd)
 # blending
+
+os.system("python3 overlap.py {0} {1}".format(content_img_dir,output_name))
 
 
 # future stages:
