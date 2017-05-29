567dad7c1b623708bcc03724a33a5a8fd6480e14
tingleshao <cshao@cs.unc.edu>
2017-05-28 22:42:32 -0400

    add run exp script

--- run_exp.py
+++ run_exp.py
@@ -1,7 +1,18 @@
 # runs the experiment:
 # two stages:
 
+
+neural_doodle_dir = '/Users/chongshao/dev/neural-doodle_chong'
+content_img_dir = ''
+style_img_dir = ''
+output_name = ''
 # style transfer
+# change dir
+os.chdir(neural_doodle_dir)
+os.system("python3 doodle.py --content {0} --style {1} --output {2} --phases=1 \
+--smoothness={3} --seed=\"content\" --iterations=500 --content-weight={} \
+--style-weight={} --semantic-weight={}".format(content_img_dir, style_img_dir, \
+output_name, smoothness, content_weight, style_weight, semantic_weight))
 
 # blending
 
