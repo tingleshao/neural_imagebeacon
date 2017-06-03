a80095ecd2bf36c63ce8ed73090dc2b317771ff6
tingleshao <cshao@cs.unc.edu>
2017-06-03 17:15:45 -0400

    add a file for evaluation

--- color_transform.py
+++ color_transform.py
@@ -1,2 +1,8 @@
 # transfer color of style image
 # so the result style transfer image preserves color
+
+import os
+
+
+def transform(input_style_image_name, input_content_image_name):
+    os.system("matlab -nodesktop -nosplash -r \"color_transfer('{0}', '{1}'); quit\"".format(input_content_image_name, input_style_image_name))
---
+++ evaluation.py
@@ -0,0 +1,13 @@
+# transfer color of style image
+# so the result style transfer image preserves color
+
+import os
+
+
+def main(input_style_image_name, input_content_image_name):
+    # TODO: evaluation based on SSIM
+
+
+
+if __name__ == '__main__':
+    main()
