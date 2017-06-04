479f636e914529485849cad78e1a7725f84b1784
tingleshao <cshao@cs.unc.edu>
2017-06-03 21:40:58 -0400

    save update

--- evaluation.py
+++ evaluation.py
@@ -4,9 +4,25 @@
 import os
 
 
-def main(input_style_image_name, input_content_image_name):
-    # TODO: evaluation based on SSIM
+def process_image(input_style_image_name, input_content_image_name):
+    return None
 
+
+def main():
+    # TODO: evaluation based on SSIM
+    # list all image dirs
+    img_dir = '/Users/chongshao/Downloads/test_image/'
+    building_dir = img_dir + "buildings/"
+    near_indoor_objs_dir = img_dir + "near_indoor_objs/"
+    near_outdoor_objs_dir = img_dir + "near_outdoor_objs/"
+    signs_dir = img_dir + "signs/"
+    # read image
+    # setnumber
+    building_set_num = 9
+    near_indoor_set_num = 21
+    near_outdoor_set_num = 9
+    signs_set_num = 20
+    
 
 
 if __name__ == '__main__':
