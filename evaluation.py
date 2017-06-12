df87fc13fc2c0b8d807ac365d99e4ed4a8f84bc0
tingleshao <cshao@cs.unc.edu>
2017-06-12 01:34:01 -0400

    save update

--- evaluation.py
+++ evaluation.py
@@ -2,7 +2,7 @@
 # so the result style transfer image preserves color
 
 import os
-from numpy.misc import imread, imsave
+from scipy.misc import imread, imsave
 
 from skimage import data, img_as_float
 from skimage.measure import compare_ssim as ssim
@@ -14,7 +14,7 @@ def process_image(input_style_image_name, input_content_image_name):
 
 def main():
     # list all image dirs
-    img_dir = '/Users/chongshao/Downloads/test_image/'
+    img_dir = '/home/tinnitant/Downloads/test_image/'
     building_dir = img_dir + "buildings/"
     near_indoor_objs_dir = img_dir + "near_indoor_objs/"
     near_outdoor_objs_dir = img_dir + "near_outdoor_objs/"
@@ -28,50 +28,53 @@ def main():
     near_indoor_quality = []
     near_outdoor_quality = []
     signs_quality = []
-    for i in range(building_set_num):
-        curr_dir = building_dir + "set{}/".format(i)
-        image_names = os.listdir(curr_dir)
-        image_names.sort()
-        image_name = image_names[-1]
+    run_building_test = True
+    if run_building_test:
+       for i in range(building_set_num):
+           if i > 2:
+              curr_dir = building_dir + "set{}/".format(i)
+              image_names = os.listdir(curr_dir + "/jpeg_res/")
+              image_names.sort()
+              image_name = image_names[-1]
         # retrieve the largest threshold image
-        curr_img = imread(image_name)
+              curr_img = imread(curr_dir + "/jpeg_res/" + image_name)
         # process this image
-        processed_curr_img = process_image(curr_img)
-        quality = evaluate_quality(processed_curr_img, original_img)
-        building_quality.append(quality)
-    for i in range(near_indoor_set_num):
-        curr_dir = near_indoor_objs_dir + "set{}/".format(i)
-        image_names = os.listdir(curr_dir)
-        image_names.sort()
-        image_name = image_names[-1]
+              processed_curr_img = process_image(curr_img)
+              quality = evaluate_quality(processed_curr_img, original_img)
+              building_quality.append(quality)
+#    for i in range(near_indoor_set_num):
+#        curr_dir = near_indoor_objs_dir + "set{}/".format(i)
+#        image_names = os.listdir(curr_dir)
+#        image_names.sort()
+#        image_name = image_names[-1]
         # retrieve the largest threshold image
-        curr_img = imread(image_name)
+#        curr_img = imread(image_name)
         # process this image
-        processed_curr_img = process_image(curr_img)
-        quality = evaluate_quality(processed_curr_img, original_img)
-        near_indoor_quality.append(quality)
-    for i in range(near_outdoor_set_num):
-        curr_dir = near_outdoor_objs_dir + "set{}/".format(i)
-        image_names = os.listdir(curr_dir)
-        image_names.sort()
-        image_name = image_names[-1]
+#        processed_curr_img = process_image(curr_img)
+#        quality = evaluate_quality(processed_curr_img, original_img)
+#        near_indoor_quality.append(quality)
+#    for i in range(near_outdoor_set_num):
+#        curr_dir = near_outdoor_objs_dir + "set{}/".format(i)
+#        image_names = os.listdir(curr_dir)
+#        image_names.sort()
+#        image_name = image_names[-1]
         # retrieve the largest threshold image
-        curr_img = imread(image_name)
+#        curr_img = imread(image_name)
         # process this image
-        processed_curr_img = process_image(curr_img)
-        quality = evaluate_quality(processed_curr_img, original_img)
-        near_outdoor_quality.append(quality)
-    for i in range(signs_set_num):
-        curr_dir = signs_dir + "set{}/".format(i)
-        image_names = os.listdir(curr_dir)
-        image_names.sort()
-        image_name = image_names[-1]
-        # retrieve the largest threshold image
-        curr_img = imread(image_name)
+#        processed_curr_img = process_image(curr_img)
+#        quality = evaluate_quality(processed_curr_img, original_img)
+#        near_outdoor_quality.append(quality)
+#    for i in range(signs_set_num):
+#        curr_dir = signs_dir + "set{}/".format(i)
+#        image_names = os.listdir(curr_dir)
+#        image_names.sort()
+#        image_name = image_names[-1]
+#        # retrieve the largest threshold image
+#        curr_img = imread(image_name)
         # process this image
-        processed_curr_img = process_image(curr_img)
-        quality = evaluate_quality(processed_curr_img, original_img)
-        signs_quality.append(quality)
+#        processed_curr_img = process_image(curr_img)
+#        quality = evaluate_quality(processed_curr_img, original_img)
+#        signs_quality.append(quality)
     print("building_quality" + str(building_quality))
     print("near_indoor_quality" + str(near_indoor_quality))
     print("near_outdoor_quality" + str(near_outdoor_quality))
@@ -83,17 +86,21 @@ def evaluate_quality(image_a, ref_iamge):
 
 
 def process_image(image):
-    neural_doodle_dir = '/Users/chongshao/dev/neural-doodle_chong'
-    content_img_dir = ''
+    neural_doodle_dir = '/home/tinnitant/dev/neural-doodle_chong'
+    content_img_dir = '' # TODO: fix here
     style_img_dir = ''
     output_name = ''
     # style transfer
     # change dir
+    smoothness = 0.4
+    content_weight = 0.4
+    style_weight = 0.4
+    semantic_weight = 0.4
     cwd = os.getcwd()
     os.chdir(neural_doodle_dir)
     os.system("python3 doodle.py --content {0} --style {1} --output {2} --phases=1 \
-    --smoothness={3} --seed=\"content\" --iterations=500 --content-weight={} \
-    --style-weight={} --semantic-weight={}".format(content_img_dir, style_img_dir, \
+    --smoothness={3} --seed=\"content\" --iterations=500 --content-weight={4} \
+    --style-weight={5} --semantic-weight={6}".format(content_img_dir, style_img_dir, \
     output_name, smoothness, content_weight, style_weight, semantic_weight))
 
     os.chdir(cwd)
